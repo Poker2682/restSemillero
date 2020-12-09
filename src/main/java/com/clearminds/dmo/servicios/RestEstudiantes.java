a8fcd68dcc7d27d3190e1dec3d205c2d3d484d98
Daniel Montes <daniel.montes2682@gmail.com>
2020-12-09 13:27:13 -0500

    Insertar y Actualizar estudiantes implementado

--- src/main/java/com/clearminds/dmo/servicios/RestEstudiantes.java
+++ src/main/java/com/clearminds/dmo/servicios/RestEstudiantes.java
@@ -2,10 +2,12 @@ package com.clearminds.dmo.servicios;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 
 import com.clearminds.dmo.dtos.Estudiante;
+import com.clearminds.dmo.excepciones.BDDException;
 
 @Path("/estudiantes")
 public class RestEstudiantes {
@@ -14,7 +16,24 @@ public class RestEstudiantes {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void insertar(Estudiante estudiante) {
-		System.out.println(estudiante);
+		ServicioEstudiante srvEstudiante = new ServicioEstudiante();
+		try {
+			srvEstudiante.insertarEstudiante(estudiante);
+		} catch (BDDException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Path("/actualizar")
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void actualizar(Estudiante estudiante) {
+		ServicioEstudiante srvEstudiante = new ServicioEstudiante();
+		try {
+			srvEstudiante.actualizarEstudiante(estudiante);
+		} catch (BDDException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
